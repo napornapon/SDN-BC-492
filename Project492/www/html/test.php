85e65676f8985eca1a35ced2fcbfe5360be25e84
napornapon <na_zzjusticezz@hotmail.com>
2018-09-05 19:39:00 +0700

    Edit Limit setting

--- Project492/www/html/test.php
+++ Project492/www/html/test.php
@@ -1,7 +1,6 @@
 <html>
 
-<a href="killqos.php">Kill all QoS</a><br>
-<a href="limith1.php" id="limith1">Limit H1</a><br>
+<a href="limith1.php">Limit H1</a><br>
 <a href="limith2.php">Limit H2</a><br>
 <a href="showqos.php">show QoS</a>
 
